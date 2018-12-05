4be580a8ceb0bf5db7a394121567a38c32903da8
Brett Cimbalik <bcimbali@gmail.com>
2018-12-04 23:02:11 -0600

    Add full app file structure Prior to this change, there was no app for this tutorial.
    This change starts tracking the app in Git.

---
+++ start.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+require('./models/Registration');
+const app = require('./app');
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.DATABASE, { useMongoClient: true });
+mongoose.Promise = global.Promise;
+mongoose.connection
+  .on('connected', () => {
+    console.log(`Mongoose connection open on ${process.env.DATABASE}`);
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
+const server = app.listen(3000, () => {
+  console.log(`Express is running on port ${server.address().port}`);
+});
